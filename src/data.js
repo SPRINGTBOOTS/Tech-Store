d2d61f06372c6e674c221626f14a54e23f391696
Alvaro Villadiego <alvaro.villadiego@udea.edu.co>
2022-10-28 13:26:04 -0500

    Modificación de imágenes

--- src/data.js
+++ src/data.js
@@ -1,20 +1,21 @@
+import reloj from './images/Watch GT2.jpeg'
 const data = {
   products: [
     {
       id: '1',
-      name: 'MacBook',
+      name: 'Reloj',
       price: 1400,
-      image: 'https://picsum.photos/id/180/2400/1600',
+      image: reloj,
     },
     {
       id: '2',
-      name: 'Old Car',
+      name: 'Automóvil antiguo miniatura',
       price: 2400,
       image: 'https://picsum.photos/id/111/4400/2656',
     },
     {
       id: '3',
-      name: 'W Shoes',
+      name: 'Zapatos de dama',
       price: 1000,
       image: 'https://picsum.photos/id/21/3008/2008',
     },
